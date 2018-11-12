e734c38b28643247a0e2562c69ef15b4a9866fb7
austinv11 <austinv11@gmail.com>
2018-11-11 19:32:54 -0500

    Verb loading now has a fallback in case service loading fails

--- src/main/java/plasma/xmf/verbs/Verbs.java
+++ src/main/java/plasma/xmf/verbs/Verbs.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public final class Verbs {
 
-    private final static Map<String, Verb> VERB_MAPPING = new HashMap<>();
+    private final static Map<String, Verb> VERB_MAPPING = Collections.synchronizedMap(new HashMap<>());
 
     static {
         ServiceLoader.load(Verb.class).iterator().forEachRemaining(v -> VERB_MAPPING.put(v.namespace(), v));
@@ -14,8 +14,24 @@ public final class Verbs {
         return VERB_MAPPING.keySet();
     }
 
+    @SuppressWarnings("unchecked")
     public static Verb getVerb(String namespace) {
-        return VERB_MAPPING.get(namespace);
+        Verb v = VERB_MAPPING.get(namespace);
+
+        if (v != null)
+            return v;
+
+        // Fallback to java classloading as a last ditch effort
+        try {
+            Class<? extends Verb> vc = (Class<? extends Verb>) Class.forName(namespace);
+            v = vc.newInstance();
+            VERB_MAPPING.put(namespace, v);
+            System.err.println("[XMF Parser] WARNING: The verb under the namespace '" + namespace + "' had to be loaded " +
+                    "by a fallback handler, please report this to the verb's developer to wire the service correctly!");
+            return v;
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public static Set<String> getAvailableProperties() {
